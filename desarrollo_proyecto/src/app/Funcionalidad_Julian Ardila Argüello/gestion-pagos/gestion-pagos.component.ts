f5dc557a78c5192d59cdf3b842dbc6ff1660854e
Julian Ardila <julian.ardila@uptc.edu.co>
2021-10-19 16:51:18 -0500

    funcionalidad pagos

---
+++ desarrollo_proyecto/src/app/Funcionalidad_Julian Ardila Argüello/gestion-pagos/gestion-pagos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-pagos',
+  templateUrl: './gestion-pagos.component.html',
+  styleUrls: ['./gestion-pagos.component.css']
+})
+export class GestionPagosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
